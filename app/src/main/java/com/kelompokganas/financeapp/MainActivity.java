8972440c84f961131bdd6d52edc94bcdd5279864
NanCyfeuy <adnanmuftimaulana0@gmail.com>
2026-04-16 12:53:49 +0800

    tes

--- app/src/main/java/com/kelompokganas/financeapp/MainActivity.java
+++ app/src/main/java/com/kelompokganas/financeapp/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         // Mengatur layout utama dari res/layout/activity_main.xml
         setContentView(R.layout.activity_main);
 
-        // Jika activity baru pertama kali dibuat, tampilkan DashboardFragment
+        // Jika activity baru pertama kali dibuat, tampilkan DashboardFragment aphfaif
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, new DashboardFragment())
